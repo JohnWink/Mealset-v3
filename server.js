ac966c258e742c12f76769617c98028dea1b2ab1
Luis Pires <luisbpires@hotmail.com>
2020-06-05 17:45:08 +0100

    update

--- server.js
+++ server.js
@@ -103,10 +103,24 @@ app.use(session({
   );
 */
 
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", '*');
+  res.header("Access-Control-Allow-Credentials", true);
+  res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
+  res.header("Access-Control-Allow-Headers", 'Origin,X-Requested-With,Content-Type,Accept,content-type,application/json');
+  next();
+});
 
-app.listen(8080,function(){
-    console.log("Server running at http://localhost:8080/")
+
+let port = process.env.PORT
+
+if(port == null||port ==""){
+  port = 3000
+}
+app.listen(port,function(){
+  console.log("Server running at port", port)
 })
+
 
 
 
