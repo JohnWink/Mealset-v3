7475a497a459ce63e04ba0e802740f62e76d2d80
JohnWink <luisbpires@hotmail.com>
2021-03-20 22:06:28 +0000

    methods update

--- server.js
+++ server.js
@@ -38,10 +38,10 @@ app.use(bodyParser.json());
 app.engine('html',mustacheExpress())
 app.set('view engine', 'html')
 
-
+z
 app.use(function(req, res, next) {
   res.header("Access-Control-Allow-Origin", "*"); // update to match the domain you will make the request from
-  res.header("Access-Control-Allow-Methods", "*");
+  res.header("Access-Control-Allow-Methods", "GET,PUT,POST");
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
   next();
 });
