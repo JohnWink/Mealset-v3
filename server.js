e97cd4462bbc8c0b012ea1ce18585571ac969bc8
JohnWink <luisbpires@hotmail.com>
2021-03-21 19:57:12 +0000

    fix fix fix this is the final fix, pinky promise, I swear

--- server.js
+++ server.js
@@ -42,7 +42,7 @@ app.set('view engine', 'html')
 app.use(function(req, res, next) {
   res.setHeader("Access-Control-Allow-Origin", '*');
   res.setHeader("Access-Control-Allow-Credentials", "true");
-  res.setHeader('Access-Control-Allow-Methods', 'GET,HEAD,OPTIONS,POST,PUT');
+  res.setHeader('Access-Control-Allow-Methods', 'GET,HEAD,OPTIONS,POST,PUT,DELETE');
   //res.header("Access-Control-Allow-Headers", 'Origin,X-Requested-With,Content-Type,Accept,content-type,application/json');
   res.setHeader("Access-Control-Allow-Headers", "Access-Control-Allow-Headers, Origin,Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers");
   next();
